e7586832579a4d9f3561ae46c2571bf25d2a4116
AMIT SINGH <amit21671@gmail.com>
2025-05-07 03:53:02 +0530

    Fix: Premium Prices Updated

--- src/images/posters/posters.ts
+++ src/images/posters/posters.ts
@@ -1,117 +1,102 @@
 // @ts-nocheck
-import REAP from './REAP.JPG'
-import ALLINONE from './ALL-IN-ONE-COLLEGES.JPG';
-import AKTU from './AKTU.jpg';
-import HBTU from './HBTU.JPG';
-import MP_DTE from './MP-DTE.JPG';
-import JAC_DELHI from './JAC-DELHI.JPG';
-import MHT_SET from './MHT-SET.JPG';
-import HSTES from './HSTES.JPG';
-import IPU_GUARANTEED from './IPU-GUARANTEED.JPG';
-import WBUEE from './WBUEE.JPG';
-import COMEDK from './comedk.JPG';
-import COMBO from './COMBO.JPG';
-import TOP_BRANDED from './TOP-BRANDED-COLLEGES.JPG';
-import JAC_CHANDIGARH from './JAC-CHANDIGARH.png';
-import JAC_DELHI_SPOT from './JAC-DELHI-SPOT.png';
-import JOSAACSAB from './JOSAACSAB.png';
+import REAP from "./REAP.JPG";
+import ALLINONE from "./ALL-IN-ONE-COLLEGES.JPG";
+import AKTU from "./AKTU.jpg";
+import HBTU from "./HBTU.JPG";
+import MP_DTE from "./MP-DTE.JPG";
+import JAC_DELHI from "./JAC-DELHI.JPG";
+import MHT_SET from "./MHT-SET.JPG";
+import HSTES from "./HSTES.JPG";
+import IPU_GUARANTEED from "./IPU-GUARANTEED.JPG";
+import WBUEE from "./WBUEE.JPG";
+import COMEDK from "./comedk.JPG";
+import COMBO from "./COMBO.JPG";
+import TOP_BRANDED from "./TOP-BRANDED-COLLEGES.JPG";
+import JAC_CHANDIGARH from "./JAC-CHANDIGARH.png";
+import JAC_DELHI_SPOT from "./JAC-DELHI-SPOT.png";
+import JOSAACSAB from "./JOSAACSAB.png";
 
 export const posters = [
-
   {
-    name: 'ALL IN ONE ONLINE',
-    image: ALLINONE,
-    price: 1499,
-    subtitle: 'All In One Online',
+    name: "IPU Guaranteed Allotment Support",
+    image: IPU_GUARANTEED,
+    subtitle: "IPU Guaranteed Allotment Support",
   },
   {
-    name: 'REAP',
-    image: REAP,
-    price: 1999,
-    subtitle: 'Reap Complete Support',
+    name: "Counselling Combo",
+    image: COMBO,
+    subtitle: "Counselling Combo",
   },
   {
-    name: 'AKTU/UPTU/UPTAC TFW/FW Premium',
-    image: AKTU,
-    price: 2999,
-    subtitle: 'AKTU/UPTU/UPTAC TFW/FW Premium',
-  },
-  {
-    name: 'AKTU/UPTU/UPTAC & MMMUT & HBTU & MJPU & CSJMU',
-    image: HBTU,
-    price: 2499,
-    subtitle: 'AKTU/UPTU/UPTAC & MMMUT & HBTU & MJPU & CSJMU Special Package',
-  },
-  {
-    name: 'MP-DTE Complete Support',
-    image: MP_DTE,
-    price: 4999,
-    subtitle: 'MP-DTE Complete Support (including CLC Rounds)',
-  },
-  {
-    name: 'JAC DELHI',
-    image: JAC_DELHI,
+    name: "TOP BRANDED COLLEGES",
+    image: TOP_BRANDED,
     price: 9999,
-    subtitle: 'JAC-Delhi Complete Support (including Spot Round)',
+    subtitle: "Complete Support For BITS/DAIICT/THAPAR/VIT/MANIPAL/JAYPEE/SRM/KIIT/ NMIMS/K J Somaiya/D Y PATIL/SIT PUNE/MIT WPU/BIT JAIPUR",
   },
   {
-    name: 'JOSA CSAB',
+    name: "JOSAA & CSAB",
     image: JOSAACSAB,
     price: 9999,
-    subtitle: 'JOSA/CSAB Complete Support',
+    subtitle: "JOSA/CSAB Complete Support",
   },
   {
-    name: 'JAC-Delhi Spot',
+    name: "JAC DELHI",
+    image: JAC_DELHI,
+    price: 9999,
+    subtitle: "JAC-Delhi Complete Support (including Spot Round)",
+  },
+  {
+    name: "JAC-Delhi Spot",
     image: JAC_DELHI_SPOT,
     price: 4999,
-    subtitle: 'JAC-Delhi Spot Round Support Only',
+    subtitle: "JAC-Delhi Spot Round Support Only",
   },
   {
-    name: 'JAC CHANDIGARH',
-    image: JAC_CHANDIGARH,
-    price: 1999,
-    subtitle: 'JAC-Chandigarh Complete Support',
-  },
-  {
-    name: 'MHT-CET Complete Support',
-    image: MHT_SET,
-    price: 4999,
-    subtitle: 'MHT-CET Complete Support (including ILC Rounds',
-  },
-  {
-    name: 'HSTES Complete Support',
+    name: "HSTES Complete Support",
     image: HSTES,
-    price: 1999,
-    subtitle: 'HSTES Complete Support',
+    price: 9999,
+    subtitle: "HSTES Complete Support",
   },
   {
-    name: 'WBJEE Complete Support',
-    image: WBUEE,
-    price: 1999,
-    subtitle: 'WBJEE Complete Support (including Spot Rounds)',
+    name: "MHT-CET Complete Support",
+    image: MHT_SET,
+    price: 9999,
+    subtitle: "MHT-CET Complete Support (including ILC Rounds)",
   },
   {
-    name: 'COMEDK Complete Support',
+    name: "COMEDK Complete Support",
     image: COMEDK,
     price: 4999,
-    subtitle: 'COMEDK Complete Support',
+    subtitle: "COMEDK Complete Support",
   },
   {
-    name: 'IPU Guaranteed Allotment Support',
-    image: IPU_GUARANTEED,
-    subtitle: 'IPU Guaranteed Allotment Support',
+    name: "AKTU/UPTU/UPTAC & MMMUT & HBTU & MJPU & CSJMU",
+    image: HBTU,
+    price: 9999,
+    subtitle: "AKTU/UPTU/UPTAC & MMMUT & HBTU & MJPU & CSJMU Special Package",
   },
   {
-    name: 'Counselling Combo',
-    image: COMBO,
-    subtitle: 'Counselling Combo',
+    name: "MP-DTE Complete Support",
+    image: MP_DTE,
+    price: 9999,
+    subtitle: "MP-DTE Complete Support (including CLC Rounds)",
   },
-
   {
-    name: 'TOP BRANDED COLLEGES',
-    image: TOP_BRANDED,
-    subtitle: 'Complete Support For Top Brand Colleges',
-  }
+    name: "JAC CHANDIGARH",
+    image: JAC_CHANDIGARH,
+    price: 4999,
+    subtitle: "JAC-Chandigarh Complete Support",
+  },
+  {
+    name: "REAP",
+    image: REAP,
+    price: 4999,
+    subtitle: "Reap Complete Support",
+  },
+  {
+    name: "WBJEE Complete Support",
+    image: WBUEE,
+    price: 4999,
+    subtitle: "WBJEE Complete Support (including Spot Rounds)",
+  },
 ];
-
-
